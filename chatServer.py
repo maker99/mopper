c5a75218c93821e65c0e830dfcf372ef1211204e
maker99 <maker99@noreply.github.com>
2022-01-01 20:23:25 +0100

    bug fix in except

--- chatServer.py
+++ chatServer.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
             pass
 
         # except (KeyboardInterrupt, SystemExit):
-        except ():
+        except:
             serversock.close()
             break
 
