80f0d0eb694d76afd4905f60d44de05308a1d114
maker99 <maker99@noreply.github.com>
2022-03-07 19:07:17 +0100

    added new names, preparing speed diffs

--- qsobot.py
+++ qsobot.py
@@ -40,31 +40,34 @@ class QsoBot:
             # 'DEFAULT' : [r'default','fb hw?'],
             },
         'MIDI': {
-            'CALL'       : [r'(call).*(\s+(hr|here|is))*\s+(?P<CALL>\S+)'    , 'dr {OP} , my call is {OWN_CALL} {OWN_CALL} {OWN_CALL}'           ],
-            'CALL_SIMPLE': [r'(call)'                                      , 'dr om ,  my call is {OWN_CALL} {OWN_CALL} {OWN_CALL}'            ],
-            'NAME'       : [r'(op|name|am).*(\s+(hr|here|is))*\s+(?P<OP>\S+)', 'dr {OP} , my name is {OWN_NAME} {OWN_NAME} {OWN_NAME}'           ],
-            'NAME_SIMPLE': [r'(op|name|am)'                                , 'dr om , ur name? = my name is {OWN_NAME} {OWN_NAME} {OWN_NAME}'  ],
-            'QTH'        : [r'(loc|qth).*(\s+(hr|here|is))*\s+(?P<QTH>\S+)'  , 'dr {OP} fm {QTH} , my qth is {OWN_QTH} {OWN_QTH} {OWN_QTH}'    ],
-            'QTH_SIMPLE' : [r'(loc|qth)'                                   , 'my qth is {OWN_QTH} {OWN_QTH} {OWN_QTH} = ur qth?'             ],
-            'WX'         : [r'(wx).*(\s+(hr|here|is))*\s+(?P<WX>\S+)'        , 'wx hr is {OWN_WX} {OWN_WX} {OWN_WX}'                         ],
-            'WX_SIMPLE'  : [r'(wx)'                                        , 'wx hr is {OWN_WX} {OWN_WX} {OWN_WX}'                         ],
-            'TMP'        : [r'(tmp).*(\s+(hr|here|is))*\s+(?P<TMP>\S+(c|f)?)', 'temp hr is {OWN_TEMP} {OWN_TEMP} {OWN_TEMP}'                     ],
-            'TMP_SIMPLE' : [r'(temp)'                                      , 'temp hr is {OWN_TEMP} {OWN_TEMP}{OWN_TEMP}'                     ],
-            'RIG'        : [r'(rig).*(\s+(hr|here|is))*\s+(?P<RIG>\S+)'      , 'my rig is {OWN_RIG} {OWN_RIG} {OWN_RIG}'                       ],
-            'RIG_SIMPLE' : [r'(rig)'                                       , 'my rig is {OWN_RIG} {OWN_RIG} {OWN_RIG}'                       ],
-            'ANT'        : [r'(ant).*(\s+(hr|here|is))*\s+(?P<ANT>\S+)'      , 'my ant is {OWN_ANT} {OWN_ANT} {OWN_ANT}'                       ],
-            'ANT_SIMPLE' : [r'(ant)'                                       , 'my ant is {OWN_ANT} {OWN_ANT} {OWN_ANT}'                       ],
-            'RST'        : [r'(rst).*(\s+(is))*\s+(?P<OWN_RST>\S+)'          , 'ur rst is {UR_RST} {UR_RST} {UR_RST}'                        ],
-            'RST_SIMPLE' : [r'(rst)'                                       , 'ur rst is {UR_RST} {UR_RST} {UR_RST} = dr {OP}, hw cpy?'     ],
-            'GETALL2'    : [r'\?\?\?'                                      , 'wx {OWN_WX} {OWN_WX} = temp {OWN_TEMP} {OWN_TEMP} = rig {OWN_RIG} {OWN_RIG} = ant {OWN_ANT} {OWN_ANT}' ],
-            'GETALL1'    : [r'\?\?'                                        , 'dr {OP} = rst {UR_RST} {UR_RST} = qth {OWN_QTH} {OWN_QTH} = name {OWN_NAME} {OWN_NAME}' ],
-            'REPEAT'     : [r'\?'                                          , '{LAST_MSG}'                                ],
-            'DEFAULT'    : [r'.'                                           , 'dr {OP} , hw cpy?'                         ],
+            'CALL'       : [r'\b(call)\b.*\b(?P<CALL>\S+)\b'        , 'dr {OP} , my call is {OWN_CALL} {OWN_CALL} {OWN_CALL}'             ],
+            'CALL_SIMPLE': [r'\b(call)\b'                           , 'dr om ,  my call is {OWN_CALL} {OWN_CALL} {OWN_CALL}'              ],
+            'NAME'       : [r'\b(op|name|am)\b.*\b(?P<OP>\w{3,})\b' , 'dr {OP} , my name is {OWN_NAME} {OWN_NAME} {OWN_NAME}'             ],
+            'NAME_SIMPLE': [r'\b(op|name|am)\b'                     , 'dr om , ur name? = my name is {OWN_NAME} {OWN_NAME} {OWN_NAME}'    ],
+            'QTH'        : [r'\b(loc|qth)\b.*\b(?P<QTH>\w{3,})\b'   , 'dr {OP} fm {QTH} , my qth is {OWN_QTH} {OWN_QTH} {OWN_QTH}'        ],
+            'QTH_SIMPLE' : [r'\b(loc|qth)\b'                        , 'my qth is {OWN_QTH} {OWN_QTH} {OWN_QTH} = ur qth?'                 ],
+            'WX'         : [r'\b(wx)\b.*\b(?P<WX>\w{3,})\b'         , 'wx hr is {OWN_WX} {OWN_WX} {OWN_WX}'                               ],
+            'WX_SIMPLE'  : [r'\b(wx)\b'                             , 'wx hr is {OWN_WX} {OWN_WX} {OWN_WX}'                               ],
+            'TMP'        : [r'\b(tmp|temp)\b.*\b(?P<TMP>(-|\+|minus|plus\s*)?\w+\s*(c|f)?)\b' , 'temp hr is {OWN_TEMP} {OWN_TEMP} {OWN_TEMP}'                       ],
+            'TMP_SIMPLE' : [r'\b(tmp|temp)\b'                       , 'temp hr is {OWN_TEMP} {OWN_TEMP} {OWN_TEMP}'                       ],
+            'RIG'        : [r'\b(rig)\b.*\b(?P<RIG>\w{3,})\b'       , 'my rig is {OWN_RIG} {OWN_RIG} {OWN_RIG}'                           ],
+            'RIG_SIMPLE' : [r'\b(rig)\b'                            , 'my rig is {OWN_RIG} {OWN_RIG} {OWN_RIG}'                           ],
+            'ANT'        : [r'\b(ant)\b.*\b(?P<ANT>\w{3,})\b'       , 'my ant is {OWN_ANT} {OWN_ANT} {OWN_ANT}'                           ],
+            'ANT_SIMPLE' : [r'\b(ant)\b'                            , 'my ant is {OWN_ANT} {OWN_ANT} {OWN_ANT}'                           ],
+            'RST'        : [r'\b(rst)\b.*\b(?P<OWN_RST>\w{3,})\b'   , 'ur rst is {UR_RST} {UR_RST} {UR_RST}'                              ],
+            'RST_SIMPLE' : [r'\b(rst)\b'                            , 'ur rst is {UR_RST} {UR_RST} {UR_RST} = dr {OP} , hw cpy?'          ],
+            'GETALL2'    : [r'\?\?\?'                               , 'wx {OWN_WX} {OWN_WX} = temp {OWN_TEMP} {OWN_TEMP} = rig {OWN_RIG} {OWN_RIG} = ant {OWN_ANT} {OWN_ANT}' ],
+            'GETALL1'    : [r'\?\?'                                 , 'dr {OP} = rst {UR_RST} {UR_RST} = qth {OWN_QTH} {OWN_QTH} = name {OWN_NAME} {OWN_NAME}' ],
+            'REPEAT'     : [r'\?'                                   , '{LAST_MSG}'                                                        ],
+            'QRS'        : [r'qrs'                                  , 'slower'                                                        ],
+            'QRQ'        : [r'qrq'                                  , 'faster'                                                        ],
+            'QRZ'        : [r'qrz'                                  , '{OWN_CALL} {OWN_CALL} {OWN_CALL}'                                                        ],
+            'DEFAULT'    : [r'.'                                    , 'dr {OP} , hw cpy?'                                                 ],
         }
     }
 
-    msg_break_re = r'.*=.*'
-    msg_break_char = r'='
+    msg_break_re      = r'.*=.*'
+    msg_break_char    = r'='
     msg_break_in_char = r'B'       # <bk> break in char
     msg_go_ahead_char = r'N'       # <kn> go ahead
 
@@ -75,14 +78,15 @@ class QsoBot:
             'CALL'    : '',
             'OWN_RST' : '',
             #----------------
-            'OWN_NAME': 'ger,dan,rob,tom,fred,hans,gerd,',
-            'OWN_WX'  : 'sunny, cloudy     ,rainy,foggy,snowy,windy,chilly,',
+            'OWN_NAME': 'ann,bea,ali,ake,bee,bot,buzz,bat,len,han,ted,zoe,may,april,fan,fran,ger,dan,rob,tom,fred,hans,gerd,bob,will,fritz,jan,alan,adam,andre,bert,ben,tim,ken,marc,matt,tom,val,zoe,gill,',
+            'OWN_WX'  : 'sunny, cloudy ,rainy,foggy,snowy,windy,chilly,',
             'OWN_RIG' : 'qrp, homebrew,ic 7300,ic705,ftdx10',
-            'OWN_ANT' : 'jpole,dipole,random,kelemen,endfed,gp,hb9cv',
+            'OWN_ANT' : 'jpole,dipole,random,kelemen,endfed,gp,hb9cv,trap dipole,flagpole,vertical,',
             'OWN_TEMP': 'minus 10c, 0c, 5c, 21c,31c',
-            'OWN_QTH' : 'bristol,kent,london,aachen,wuerselen,herzogenrath,bonn,koeln',
+            'OWN_QTH' : 'bristol,kent,london,aachen,wuerselen,simmerath,brand,kiev,herzogenrath,bonn,koeln,cologne,bern,hamburg,',
             'OWN_CALL': 'm0iv,da1bc,db2cd,dd3ef,de4fg,ia3tv',
-            'UR_RST'  : '599,509,518,579,589'
+            'UR_RST'  : '599,509,518,579,589',
+            'SPEED_DIFF'  : '0',
         }
 
     
@@ -108,6 +112,7 @@ class QsoBot:
         Returns:
             cleaned list: [description]
         """        
+        logging.debug(f"split_and_clean: {input}")
         return [x.strip() for x in input.split(',') if x.strip()]
 
 
